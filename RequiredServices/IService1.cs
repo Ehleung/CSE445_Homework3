5fc86d2179d6506d5144085c704d19d8ec39f0b2
Ehleung <Ellery.h.leung@gmail.com>
2017-03-22 14:40:10 -0700

    Clean Up Default Settings

--- RequiredServices/IService1.cs
+++ RequiredServices/IService1.cs
@@ -8,7 +8,6 @@ using System.Text;
 
 namespace RequiredServices
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
     [ServiceContract]
     public interface IService1
     {
@@ -17,36 +16,5 @@ namespace RequiredServices
 
         [OperationContract]
         string[] getWsOperations(string url);
-
-        [OperationContract]
-        string GetData(int value);
-
-        [OperationContract]
-        CompositeType GetDataUsingDataContract(CompositeType composite);
-
-        // TODO: Add your service operations here
-    }
-
-
-    // Use a data contract as illustrated in the sample below to add composite types to service operations.
-    [DataContract]
-    public class CompositeType
-    {
-        bool boolValue = true;
-        string stringValue = "Hello ";
-
-        [DataMember]
-        public bool BoolValue
-        {
-            get { return boolValue; }
-            set { boolValue = value; }
-        }
-
-        [DataMember]
-        public string StringValue
-        {
-            get { return stringValue; }
-            set { stringValue = value; }
-        }
     }
 }
--- RequiredServices/Service1.svc.cs
+++ RequiredServices/Service1.svc.cs
@@ -24,25 +24,7 @@ namespace RequiredServices
 
         public string getReview(string vendorName)
         {
-            asdfasdafasd
-        }
-
-        public string GetData(int value)
-        {
-            return string.Format("You entered: {0}", value);
-        }
-
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
-        {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
+         
         }
     }
 }
